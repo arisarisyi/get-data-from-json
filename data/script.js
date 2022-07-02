b20c44fda39ca94b0164f00c43f95fa363eab4be
arisarisyi <alarisyi@gmail.com>
2022-07-02 19:57:23 +0700

    Serch food by category

--- data/script.js
+++ data/script.js
@@ -1,5 +1,5 @@
 function tampilkanMenu() {
-  $.getJSON("./data/donuts.json", function (data) {
+  $.getJSON("data/donuts.json", function (data) {
     let menu = data.menu
     $("#daftar-menu").empty()
     $.each(menu, function (i, data) {
@@ -18,3 +18,40 @@ function tampilkanMenu() {
   })
 }
 tampilkanMenu()
+
+$(".nav-link").on("click", function () {
+  $(".nav-link").removeClass("active")
+  $(this).addClass("active")
+
+  let kategori = $(this).html()
+  //bacanya jquery cari h1 ganti htmlnya dengan kategori
+  $("h1").html(kategori)
+
+  if (kategori == "All Menu") {
+    tampilkanMenu()
+    return
+  }
+
+  $.getJSON("data/donuts.json", function (data) {
+    let menu = data.menu
+    let content = ""
+
+    $.each($(menu).get().reverse(), function (i, data) {
+      if (data.kategori == kategori.toLowerCase()) {
+        content +=
+          '<div class="col"><div class="card mb-3"style="width: 18rem;"><img src="img/' +
+          data.gambar +
+          ' " class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' +
+          data.nama +
+          '</h5><p class="card-text">' +
+          data.deskripsi +
+          '</p><h5 class="card-title">' +
+          data.harga +
+          '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>'
+      }
+    })
+
+    //batas
+    $("#daftar-menu").html(content)
+  })
+})
